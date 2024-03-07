10153f0ac7675d391f77d13466056b888ba49606
benlin-pt7278 <benlin-pt7278@benilin-pt7278>
2024-03-07 17:31:22 +0530

    Second commit

--- src/task/basic/programming/SingletonExample.java
+++ src/task/basic/programming/SingletonExample.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class SingletonExample implements Cloneable,Serializable{
 	
-	//make not serializable
 	
 	private static final long serialVersionUID = -570499928678063818L;
 
